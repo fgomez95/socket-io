7de9b79147551a9d53273cbff2768a4f1ab7903a
Fernando G <kev9504@gmail.com>
2019-04-08 13:18:57 +0000

    initial socket config

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
-
+var socket = require('socket.io');
 // define app 
 var app = express();
 var server = app.listen(8080, function(){
    console.log('listening to port 8080'); 
 });
 
-
+//Static Files
 app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('someone access the chat');
+});
---
+++ public/chat.js
@@ -0,0 +1,3 @@
+//Make connection 
+/* global io */
+var socket = io.connect('https://ide50-kev9504.c9users.io');
